03699455f24fba13708443255fef694d62402368
AndersonChart <anderson05chacon@gmail.com>
2025-09-15 23:05:12 -0400

    Mejora de interfaz, arreglos de inicio

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
+
 session_start();    #Iniciar las sesiones
+
+#Controlar el caché para medidas de seguridad
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 #Enrutador que determina que página cargar, por defecto el login
 $vista = isset($_GET['vista']) ? $_GET['vista'] : 'login';
 $archivo = "vistas/$vista.php";
-$rutas_publicas = ['login', 'login_registro', 'login_datos'];
+$rutas_publicas = ['login', 'login_registro'];
 
 if (!in_array($vista, $rutas_publicas) && empty($_SESSION["id"])) {
     header("Location: index.php?vista=login");
--- vistas/login.php
+++ vistas/login.php
@@ -1,28 +1,25 @@
-<body class="bg-white d-flex justify-content-center align-items-center vh-100">
-    <form action="index.php?action=login" method="POST" autocomplete="off" class="p-4 rounded shadow-sm border" style="width: 100%; max-width: 400px; background-color: #fff;">
-        <fieldset>
-        <legend class="text-danger text-center fs-4 mb-3">Iniciar sesión</legend>
+<body class="login_background">
+    <form action="index.php?action=login" method="POST" autocomplete="off" class="login_container">
+        <fieldset class="login_fieldset">
+        <legend class="login_title">Iniciar sesión</legend>
 
-        <div class="mb-3">
-            <label for="nombre" class="form-label">Nombre de usuario/Correo electrónico</label>
-            <input type="text" id="nombre" name="usuario_usuario" class="form-control" required>
+        <div class="login_block_content">
+            <label for="nombre" class="login_label">Nombre de usuario/Correo electrónico</label>
+            <input type="text" id="nombre" name="usuario_usuario" class="login_input">
         </div>
 
-        <div class="mb-3">
-            <label for="password" class="form-label">Contraseña</label>
-            <input type="password" id="password" name="usuario_clave" class="form-control" required>
+        <div class="login_block_content">
+            <label for="password" class="login_label">Contraseña</label>
+            <input type="password" id="password" name="usuario_clave" class="login_input">
         </div>
 
-        <div class="mb-3">
-            <input type="submit" value="Iniciar" name="enviar" class="btn btn-danger w-100">
-        </div>
+            <input type="submit" value="Iniciar" name="enviar" class="login_submit">
 
-        <div class="text-center">
-            <a href="index.php?vista=login_registro" class="btn btn-link text-danger">No tengo cuenta</a><br>
-            <a href="index.php?vista=login_datos" class="btn btn-link text-danger">He olvidado mi usuario/contraseña</a>
-        </div>
+            <a href="index.php?vista=login_registro" class="login_link">No tengo cuenta</a>
+
         </fieldset>
     </form>
+
 
 <?php include "php/iniciar_sesion.php"; ?>
 
