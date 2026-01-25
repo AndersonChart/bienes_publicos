553d36a7102fcab3af0d3109a38ed53b5134cc2c
AndersonChart <anderson05chacon@gmail.com>
2026-01-25 10:19:19 -0400

    MEJORAS

--- include/navbar.php
+++ include/navbar.php
@@ -20,20 +20,20 @@
         </div>
         <div class="sub-main">
             <div class="menu-content" id="articulo">
-                <a href="index.php?vista=listar_articulo">Inventario</a>
+                <a class="prioridad" href="index.php?vista=listar_articulo">Inventario</a>
                 <a href="index.php?vista=listar_categoria">Categorías</a>
                 <a href="index.php?vista=listar_clasificacion">Clasificaciones</a>
                 <a href="index.php?vista=listar_marca">Marcas</a>
             </div>
             <div class="menu-content" id="asignaciones">
-                <a href="index.php?vista=listar_asignacion">Asignaciones</a>
+                <a class="prioridad" href="index.php?vista=listar_asignacion">Asignaciones</a>
                 <a href="index.php?vista=listar_personal">Personal</a>
                 <a href="index.php?vista=listar_cargo">Cargos</a>
                 <a href="index.php?vista=listar_area">Áreas</a>
             </div>
             <div class="menu-content" id="ajustes">                
-                <a href="index.php?vista=listar_recepcion">Recepción</a>
-                <a href="index.php?vista=listar_desincorporacion">Desincorporación</a>
+                <a class="prioridad" href="index.php?vista=listar_recepcion">Recepción</a>
+                <a class="prioridad" href="index.php?vista=listar_desincorporacion">Desincorporación</a>
             </div>
             <div class="menu-content" id="usuarios">
                 <a href="index.php?vista=listar_usuario">Usuarios</a>
